a3864155465ed0a44b443e286ad18d3916c96e62
Bryan Duggan <skooter500@gmail.com>
2019-02-10 21:27:49 +0000

    bla

---
+++ Assets/Bootstrap.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public class Bootstrap : MonoBehaviour
+{
+    public Mesh boidMesh;
+    public Material boidMaterial;
+
+    public int count = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+        
+        var boidArchitype = entityManager.CreateArchetype(
+            typeof(PositionComponent)
+            , typeof(RotationComponent)
+            , typeof(RenderMeshComponent)
+            );
+
+
+        for (int i = 0; i < count; i ++)
+        {
+            var boid = entityManager.CreateEntity(boidArchitype);
+            entityManager.SetComponentData(boid, new PositionComponent()
+            {
+                Value = new Position()
+            }
+            );
+
+        // Update is called once per frame
+            void Update()
+    {
+        
+    }
+}
