15f5cb1083fb260908436ce749ee3032914bf367
skooter500 <skooter500@gmail.com>
2019-02-13 21:02:57 +0000

    bla

---
+++ Assets/Bootstrap.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+using Unity.Transforms;
+using Unity.Rendering;
+
+public class Bootstrap : MonoBehaviour
+{
+    private EntityArchetype cubeArchitype;
+    private EntityManager entityManager;
+    
+    private RenderMesh renderMesh;
+    public Mesh mesh;
+    public Material material;
+
+    Entity CreateCube(Vector3 pos, Quaternion q)
+    {
+        Entity entity = entityManager.CreateEntity(cubeArchitype);
+
+        Position p = new Position();
+        p.Value = pos;
+
+        Rotation r = new Rotation();
+        r.Value = q;
+
+        entityManager.SetComponentData(entity, p);
+        entityManager.SetComponentData(entity, r);
+
+        entityManager.AddSharedComponentData(entity, renderMesh);
+        return entity;
+    }
+
+    int numCubes = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+        cubeArchitype = entityManager.CreateArchetype(
+            typeof(Position),
+            typeof(Rotation)
+        );
+
+        renderMesh = new RenderMesh();
+        renderMesh.mesh = mesh;
+        renderMesh.material = material;
+
+        for (int i = 0; i < numCubes; i++)
+        {
+            Vector3 pos = Random.insideUnitSphere * 100;
+            CreateCube(transform.position + pos, Quaternion.identity);
+        }
+
+    }
+}
---
+++ Assets/CubeJobSystem.cs
@@ -0,0 +1,85 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class CubeJobSystem : JobComponentSystem
+{
+    struct CubeJob : IJobProcessComponentData<Position, Rotation>
+    {
+        [ReadOnly] public float deltaTime;
+
+        public void Execute(ref Position p, ref Rotation q)
+        {
+            Vector3 pos = p.Value;
+            pos.y += deltaTime;
+            p.Value = pos;
+        }
+    }
+
+    struct CountNeighboursJob : IJobParallelFor
+    {
+        public ComponentDataArray<Position> positions;
+
+        [NativeDisableParallelForRestriction]
+        public NativeArray<int> counts;
+        public float neighbourDistance;
+        public void Execute(int index)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                int count = 0;
+                if (i != index)
+                {
+                    if (Vector3.Distance(positions[index].Value, positions[i].Value) < neighbourDistance)
+                    {
+                        count++;
+                    }
+                }
+                counts[index] = count;
+            }
+            throw new System.NotImplementedException();
+        }
+    }
+
+    private struct Data
+    {
+        public readonly int Length;
+        public EntityArray Entities;
+        public ComponentDataArray<Position> positions;
+    }
+
+    [Inject] private Data data;
+
+    private NativeArray<int> counts;
+
+    protected override void OnCreateManager()
+    {
+        counts = new NativeArray<int>(100, Allocator.Persistent);
+    }
+
+    protected override void OnDestroyManager()
+    {
+        counts.Dispose();
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new CubeJob()
+        {
+            deltaTime = Time.deltaTime
+        };
+
+        var cnj = new CountNeighboursJob()
+        {
+            counts = this.counts
+            , positions = this.data.positions
+            , neighbourDistance = 20
+        };
+
+        return cnj.Schedule(data.Length, 10, inputDeps);
+    }
+}
