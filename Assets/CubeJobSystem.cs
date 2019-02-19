9720e73e35583697c3c14b6048ca21f1a3f8846e
skooter500 <skooter500@gmail.com>
2019-02-19 09:15:19 +0000

    bla

--- Assets/CubeJobSystem.cs
+++ Assets/CubeJobSystem.cs
@@ -44,7 +44,6 @@ public class CubeJobSystem : JobComponentSystem
         }
         
     }
-    /*
     struct CountNeighboursJob : IJobParallelFor
     {        
         [NativeDisableParallelForRestriction]
