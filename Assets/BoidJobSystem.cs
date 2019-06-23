262f3751fcdd1ea2d62e45aa73fc3e576191f193
skooter500 <skooter500@gmail.com>
2019-06-23 17:03:14 +0100

    before testing new system

--- Assets/BoidJobSystem.cs
+++ Assets/BoidJobSystem.cs
@@ -10,6 +10,7 @@ using UnityEngine;
 
 public class BoidJobSystem : JobComponentSystem
 {
+    public static BoidJobSystem Instance;
     static public bool checkNaN(Quaternion v)
     {
         if (float.IsNaN(v.x) || float.IsInfinity(v.x))
@@ -89,73 +90,7 @@ public class BoidJobSystem : JobComponentSystem
         }
     }
 
-    [BurstCompile]
-    struct HeadJob : IJobProcessComponentData<Head, Position, Rotation>
-    {
-        [NativeDisableParallelForRestriction]
-        public NativeArray<Vector3> positions;
-
-        [NativeDisableParallelForRestriction]
-        public NativeArray<Quaternion> rotations;
-
-        [NativeDisableParallelForRestriction]
-        public NativeArray<float> speeds;
-
-        public float amplitude;
-        public float frequency;
-        public float size;
-        public float dT;
-
-        public void Execute(ref Head h, ref Position p, ref Rotation r)
-        {
-            Vector3 up = Vector3.up;
-            Quaternion q = rotations[h.spineId] * Quaternion.AngleAxis(Mathf.Sin(h.theta) * amplitude, up);
-
-            // Calculate the center point of the head
-            Vector3 pos = positions[h.spineId]
-                + rotations[h.spineId] * (Vector3.forward * size * 0.5f)
-                + q * (Vector3.forward * size * 0.5f);
-
-            p.Value = pos;
-            r.Value = q;
-            
-            h.theta += frequency * dT * Mathf.PI * 2.0f * speeds[h.boidId];
-        }
-    }
-
-    [BurstCompile]
-    struct TailJob : IJobProcessComponentData<Tail, Position, Rotation>
-    {
-        [NativeDisableParallelForRestriction]
-        public NativeArray<Vector3> positions;
-
-        [NativeDisableParallelForRestriction]
-        public NativeArray<Quaternion> rotations;
-
-        [NativeDisableParallelForRestriction]
-        public NativeArray<float> speeds;
-
-        public float amplitude;
-        public float frequency;
-        public float size;
-        public float dT;
-
-        public void Execute(ref Tail t, ref Position p, ref Rotation r)
-        {
-            Vector3 up = Vector3.up;
-            Quaternion q = rotations[t.spineId] * Quaternion.AngleAxis(Mathf.Sin(-t.theta) * amplitude, up);
-            // Calculate the center point of the tail
-
-            //Vector3 pos = positions[t.spineId] - q * (Vector3.forward * size * 0.5f);
-            Vector3 pos = positions[t.spineId]
-                - rotations[t.spineId] * (Vector3.forward * size * 0.5f)
-                - q * (Vector3.forward * size * 0.5f);
-                
-            p.Value = pos;
-            r.Value = q;
-            t.theta += frequency * dT * Mathf.PI * 2.0f * speeds[t.boidId];
-        }
-    }
+    
 
     [BurstCompile]
     struct SeperationJob : IJobProcessComponentData<Boid, Seperation>
@@ -596,6 +531,7 @@ public class BoidJobSystem : JobComponentSystem
 
     protected override void OnCreateManager()
     {
+        Instance = this;
         bootstrap = GameObject.FindObjectOfType<BoidBootstrap>();
 
         // Want to use this but it hangs when I try and access it
@@ -735,39 +671,12 @@ public class BoidJobSystem : JobComponentSystem
         };
         var boidHandle = boidJob.Schedule(this, fleeHandle);
 
-        // Animate the head and tail
-        var headJob = new HeadJob()
-        {
-            positions = SpineSystem.Instance.positions,
-            rotations = SpineSystem.Instance.rotations,
-            speeds = this.speeds,
-            dT = Time.deltaTime * bootstrap.speed,
-            amplitude = bootstrap.headAmplitude,
-            frequency = bootstrap.animationFrequency,
-            size = bootstrap.size
-        };
-
-        var headHandle = headJob.Schedule(this, boidHandle);// Animate the head and tail
-
-        var tailJob = new TailJob()
-        {
-            positions = SpineSystem.Instance.positions,
-            rotations = SpineSystem.Instance.rotations,
-            speeds = this.speeds,
-            dT = Time.deltaTime * bootstrap.speed,
-            amplitude = bootstrap.tailAmplitude,
-            frequency = bootstrap.animationFrequency,
-            size = bootstrap.size
-        };
-
-        var tailHandle = tailJob.Schedule(this, headHandle);
-
         // Copy back to the entities 
         var cfj = new CopyTransformsFromJob()
         {
             positions = this.positions,
             rotations = this.rotations
         };
-        return cfj.Schedule(this, tailHandle);
+        return cfj.Schedule(this, boidHandle);
     }
 }
